a77704d9b59c0d649b584ed62f66c0d9b0f68355
dillan <dillanhoyosmusic@gmail.com>
2019-09-23 19:58:00 -0400

    seeya change

--- seeya.c
+++ seeya.c
@@ -5,6 +5,6 @@ int main()
     printf("Ask Me How Do I Feel?\a\n");
     printf("Ask Me Now\a\n");
     printf("Tha-it hWe're Cozy\a\n");
-    printf("And Clinging\a\n");
+    printf("And Clingkmjjing\a\n");
     return 0;
 }
