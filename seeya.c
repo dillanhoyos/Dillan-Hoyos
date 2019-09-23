1723229d6f7bae6906da713629da35c063b3948d
dillan <dillanhoyosmusic@gmail.com>
2019-09-23 19:51:43 -0400

    that it

---
+++ seeya.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ask Me How Do I Feel?\a\n");
+    printf("Ask Me Now\a\n");
+    printf("Thatit We're Cozy\a\n");
+    printf("And Clinging\a\n");
+    return 0;
+}
