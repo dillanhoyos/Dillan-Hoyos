ff10e513f1f3505320681075cc260e39425bb4c4
dillan <dillanhoyosmusic@gmail.com>
2019-09-30 08:03:01 -0400

    ds

---
+++ haikuh/haiku2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("A summer river\a\n");
+    printf("being crossed\a\n");
+    printf( "how pleasing with sandals in my hands!\n\a");
+    return 0;
+}
