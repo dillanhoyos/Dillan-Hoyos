e96c08c8100d0b9c6473b2d2ed9e3b3cd1586517
dillanhoyos <55099931+dillanhoyos@users.noreply.github.com>
2019-11-18 20:38:33 -0500

    bvla

---
+++ problemset.py
@@ -0,0 +1,10 @@
+def print_one():
+	print("+----+----+")
+
+
+def print_two():
+
+	print("|    |    |")
+
+
+print_one();print_two();print_two();print_two();print_two();print_one();print_two();print_two();print_two();print_two();print_one();
