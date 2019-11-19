571aac370d9f9bca5facc8958a12a3562fbe1e4a
dillanhoyos <55099931+dillanhoyos@users.noreply.github.com>
2019-11-18 20:36:16 -0500

    problemset

---
+++ week 10/problemset.py
@@ -0,0 +1,22 @@
+def print_one():
+	print("+----+----+")
+
+
+>>> def print_two():
+
+	print("|    |    |")
+
+
+>>> print_one();print_two();print_two();print_two();print_two();print_one();print_two();print_two();print_two();print_two();print_one();
++----+----+
+|    |    |
+|    |    |
+|    |    |
+|    |    |
++----+----+
+|    |    |
+|    |    |
+|    |    |
+|    |    |
++----+----+
+>>>
