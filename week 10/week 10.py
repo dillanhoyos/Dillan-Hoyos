2ae81ca793cdca6cbb8ae8c608d9a05322dc638b
dillanhoyos <55099931+dillanhoyos@users.noreply.github.com>
2019-11-18 20:32:39 -0500

    week 10

---
+++ week 10/week 10.py
@@ -0,0 +1,222 @@
+Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> #vfdasfvadfvdv
+>>> 5-6
+-1
+>>> 60+8
+68
+>>> oe
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    oe
+NameError: name 'oe' is not defined
+>>> one ==1
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    one ==1
+NameError: name 'one' is not defined
+>>> one-1
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    one-1
+NameError: name 'one' is not defined
+>>> one=1
+>>> prin////////////////////÷÷÷////.//////]]]]]Traceback (most recent call last):
+	
+SyntaxError: invalid syntax
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> empty
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    empty
+NameError: name 'empty' is not defined
+>>> shut
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    shut
+NameError: name 'shut' is not defined
+>>> printNameError: name 'shut' is not defined
+SyntaxError: invalid syntax
+>>> print('hello, world')
+hello, world
+>>> str = input('name: ")
+	    
+SyntaxError: EOL while scanning string literal
+>>> str = input('name; ')
+name; Dillan
+>>> print('hello,", str)
+      
+SyntaxError: EOL while scanning string literal
+>>> print ("hello,",str)
+hello, Dillan
+>>> pritn
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    pritn
+NameError: name 'pritn' is not defined
+>>> 
+>>> 9]
+SyntaxError: unmatched ']'
+>>> x
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+>>> x= int(input("x, "))
+x, 7
+>>> y=int
+>>> 
+>>> 
+>>> 
+9
+>>> y=int(int(input('y; "))
+		
+SyntaxError: EOL while scanning string literal
+>>> y = int(input("y: "))
+y: 9
+>>> print(f"x + y = {x + y}")
+print(f"x - y = {x - y}")
+print(f"x * y = {x * y}")
+print(f"x / y = {x / y}")
+print(f"x mod y = {x % y}")
+SyntaxError: multiple statements found while compiling a single statement
+>>> print(f"x + y = {x + y}")
+x + y = 16
+>>> print(f"x - y = {x - y}")
+x - y = -2
+>>> print(f"x mod y = {x % y}")
+x mod y = 7
+>>> print(f"x * y = {x * y}")
+x * y = 63
+>>> if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+
+    
+x is less than y
+>>> y: 10
+>>> if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+
+    
+x is less than y
+>>> x:20
+>>> if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+
+    
+x is less than y
+>>> 	def print_lyrics():
+    print("I'm gonna take my horse to the old town road.")
+    print("I'm gonna ride 'til I can't no more.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+SyntaxError: unexpected indent
+>>> def print_lyrics():
+    print("I'm gonna take my horse to the old town road.")
+    print("I'm gonna ride 'til I can't no more.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+repeat_lyrics()
+SyntaxError: invalid syntax
+>>> def print_lyrics():
+    print("I'm gonna take my horse to the old town road.")
+    print("I'm gonna ride 'til I can't no more.")
+
+
+>>> 
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+SyntaxError: invalid syntax
+>>> def print_lyrics():
+    print("I'm gonna take my horse to the old town road.")
+    print("I'm gonna ride 'til I can't no more.")
+
+
+>>> def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+>>> repeat_lyrics()
+I'm gonna take my horse to the old town road.
+I'm gonna ride 'til I can't no more.
+I'm gonna take my horse to the old town road.
+I'm gonna ride 'til I can't no more.
+>>> print ("+----+----+")
++----+----+
+>>> def print_one():
+	print("+----+----+")
+
+	
+>>> def print_two():
+
+	print("|    |    |")
+
+	
+>>> print_one();
++----+----+
+>>> print_one()
++----+----+
+>>> print_one();print_two;print_two;print_two;print_two;print_one;;print_two;print_two;print_two;print_two;print_one
+SyntaxError: invalid syntax
+>>> print_one();print_two;print_two;print_two;print_two;print_one;
+print_two;print_two;print_two;print_two;print_one;
+SyntaxError: multiple statements found while compiling a single statement
+>>> print_one();print_two;print_two;print_two;print_two;print_one;;print_two;print_two;print_two;print_two;print_one
+SyntaxError: invalid syntax
+>>> print_one();print_two;print_two;print_two;print_two;print_one;print_two;print_two;print_two;print_two;print_one
++----+----+
+<function print_two at 0x110929e50>
+<function print_two at 0x110929e50>
+<function print_two at 0x110929e50>
+<function print_two at 0x110929e50>
+<function print_one at 0x110929dc0>
+<function print_two at 0x110929e50>
+<function print_two at 0x110929e50>
+<function print_two at 0x110929e50>
+<function print_two at 0x110929e50>
+<function print_one at 0x110929dc0>
+>>> print_two
+<function print_two at 0x110929e50>
+>>> print_one();print_two();print_two();print_two();print_two();print_one();print_two();print_two();print_two();print_two();print_one();
++----+----+
+|    |    |
+|    |    |
+|    |    |
+|    |    |
++----+----+
+|    |    |
+|    |    |
+|    |    |
+|    |    |
++----+----+
+>>> 
