5d9e7c5996583e4bcabedcad757d799b1d35dfd5
dillanhoyos <55099931+dillanhoyos@users.noreply.github.com>
2019-11-18 20:39:20 -0500

    this is the oe\

--- week 10/problemset.py
+++ week 10/problemset.py
@@ -2,21 +2,9 @@ def print_one():
 	print("+----+----+")
 
 
->>> def print_two():
+def print_two():
 
 	print("|    |    |")
 
 
->>> print_one();print_two();print_two();print_two();print_two();print_one();print_two();print_two();print_two();print_two();print_one();
-+----+----+
-|    |    |
-|    |    |
-|    |    |
-|    |    |
-+----+----+
-|    |    |
-|    |    |
-|    |    |
-|    |    |
-+----+----+
->>>
+print_one();print_two();print_two();print_two();print_two();print_one();print_two();print_two();print_two();print_two();print_one();
